7f59bb8ec6a0cd38d694ab08e47a365201f258c1
oliverka <37831000+oliverka@users.noreply.github.com>
2018-04-10 11:25:08 +0200

    commit

--- app/src/main/java/app/ok/bimbomind/NewGame.java
+++ app/src/main/java/app/ok/bimbomind/NewGame.java
@@ -2,10 +2,15 @@ package app.ok.bimbomind;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * Created by o.k on 15.03.2018.
@@ -13,24 +18,39 @@ import android.widget.CheckBox;
 
 public class NewGame extends Activity {
 
-    private Button new_game;
-    private Button abort;
-    private CheckBox allow_empty, allow_multiple;
+    private Database database;
+    private CheckBox allow_empty;
+    private CheckBox allow_multiple;
+    private EditText turns;
+    private EditText colors;
+    private EditText holes;
+    private ToggleButton ai_human;
+    private ToggleButton set_guess;
+    private TextView error_text;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game);
-        new_game = (Button) findViewById(R.id.new_game_start_game);
-        abort = (Button) findViewById(R.id.new_game_abort);
+        database = new Database(this,null,null, 1);
+        Button new_game = (Button) findViewById(R.id.new_game_start_game);
+        Button abort = (Button) findViewById(R.id.new_game_abort);
         allow_empty = (CheckBox) findViewById(R.id.new_game_allow_empty);
         allow_multiple = (CheckBox) findViewById(R.id.new_game_allow_multiple);
+        turns = (EditText) findViewById(R.id.new_game_turns_input);
+        colors = (EditText) findViewById(R.id.new_game_colors_input);
+        holes = (EditText) findViewById(R.id.new_game_holes_input);
+        ai_human = (ToggleButton) findViewById(R.id.new_game_ai_human);
+        set_guess = (ToggleButton) findViewById(R.id.new_game_set_guess);
+        error_text = (TextView) findViewById(R.id.new_game_error_text);
         new_game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(NewGame.this, Game.class);
-                startActivity(intent);
-                finish();
+                if (saveStats()) {
+                    Intent intent = new Intent(NewGame.this, Game.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
         abort.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +65,7 @@ public class NewGame extends Activity {
     }
 
     private void abort() {
+
     }
 
     @Override
@@ -53,5 +74,44 @@ public class NewGame extends Activity {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
         finish();
+    }
+
+    private boolean saveStats() {
+        try {
+            error_text.setText("");
+            boolean turns1 = true, colors1 = true, holes1 = true;
+            if (Integer.parseInt(String.valueOf(turns.getText())) > 12) {
+                turns1 = false;
+                error_text.setTextColor(Color.RED);
+                error_text.append("Only less than 13 turns are allow\n");
+            }
+            if (Integer.parseInt(String.valueOf(colors.getText())) != 5 &&
+                    Integer.parseInt(String.valueOf(colors.getText())) != 6 &&
+                    Integer.parseInt(String.valueOf(colors.getText())) != 8) {
+                colors1 = false;
+                error_text.setTextColor(Color.RED);
+                error_text.append("Only 5, 6 or 8 different colors are allow\n");
+            }
+            if (Integer.parseInt(String.valueOf(holes.getText())) != 5 &&
+                    Integer.parseInt(String.valueOf(holes.getText())) != 6 &&
+                    Integer.parseInt(String.valueOf(holes.getText())) != 8) {
+                holes1 = false;
+                error_text.setTextColor(Color.RED);
+                error_text.append("Only 5, 6 or 8 holes are allow\n");
+            }
+            if (turns1 && colors1 && holes1) {
+                database.setPreference(Database.PREFERENCE_SAVEGAME_ALLOWEMPTY, String.valueOf(allow_empty.isChecked()));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_ALLOWMULTIPLE, String.valueOf(allow_multiple.isChecked()));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_MAXTURNS, Integer.parseInt(String.valueOf(turns.getText())));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_COLORCOUNT, Integer.parseInt(String.valueOf(colors.getText())));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_HOLES, Integer.parseInt(String.valueOf(holes.getText())));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_AI, String.valueOf(ai_human.isChecked()));
+                database.setPreference(Database.PREFERENCE_SAVEGAME_SET, String.valueOf(set_guess.isChecked()));
+                return true;
+            }
+        }catch (NumberFormatException e) {
+            Toast.makeText(this, "Please enter some values",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
